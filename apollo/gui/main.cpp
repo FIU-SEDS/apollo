65de3ac3c4ca9e97f33f3ada3a748e9ea57a1d7c
Eriel Cabrera <eriel.dev@pm.me>
2026-02-07 23:41:41 -0500

    added slider/text
    - refactored main dir apollo/
    - changed views in CMakeLists.txt

--- apollo/gui/main.cpp
+++ apollo/gui/main.cpp
@@ -1,3 +1,5 @@
+#include <cstdint>
+#include <cstdio>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
@@ -34,6 +36,7 @@ int main()
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 330");
 
+    float altitude = 0.0f;
     // Main loop
     while (!glfwWindowShouldClose(window))
     {
@@ -42,13 +45,23 @@ int main()
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
+        ImGui::SetNextWindowSize(ImVec2(0.0f, 0.0f));
 
         // UI
         ImGui::Begin("Apollo UI");
         ImGui::Text("ImGui is working!");
-        if (ImGui::Button("Press me"))
-            printf("Button pressed\n");
+        // if (ImGui::Button("Press me"))
+         //  printf("Button pressed\n");
+         //
+        // Slider
+        ImGui::SliderFloat("altitude", &altitude, 0.0f, 10000.0f, "value = %0.3f");
+        printf("altitude value = %0.3f\n", altitude);
+
+        // text 
+        ImGui::Text("Altitude = %0.3f", altitude);
+
         ImGui::End();
+
 
         // Render
         ImGui::Render();
